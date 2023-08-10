412fb38a95de113214832ea74c64df03bb45edf9
Yue LI <yueli.7888@gmail.com>
2023-08-10 20:39:28 +1200

    modify some files and add the extra-task work

---
+++ extra-task/src/index.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+if (typeof document !== 'undefined') {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+reportWebVitals();
+
+/* eslint-enable no-undef */
